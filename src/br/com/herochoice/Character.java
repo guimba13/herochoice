84ed15c0886260e10c06785c7c9b769d9afcf86f
Guilherme Zanelato <guilherme.c.zanelato@gmail.com>
2014-12-05 22:34:44 -0200

    brasil

--- src/br/com/herochoice/Character.java
+++ src/br/com/herochoice/Character.java
@@ -1,70 +1,95 @@
 package br.com.herochoice;
 
 public class Character {
-	
 
 	private int id;
 	private String name;
 	private boolean hero;
 	private int intelligence;
 	private int strength;
+	private int speedy;
 	private int durability;
 	private int energy;
 	private int fightingSkills;
 	private int numberComics;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getNumberComics() {
 		return numberComics;
 	}
+
 	public void setNumberComics(int numberComics) {
 		this.numberComics = numberComics;
 	}
+
 	public boolean isHero() {
 		return hero;
 	}
+
 	public void setHero(boolean hero) {
 		this.hero = hero;
 	}
+
 	public int getIntelligence() {
 		return intelligence;
 	}
+
 	public void setIntelligence(int intelligence) {
 		this.intelligence = intelligence;
 	}
+
 	public int getStrength() {
 		return strength;
 	}
+
 	public void setStrength(int strength) {
 		this.strength = strength;
 	}
+
 	public int getDurability() {
 		return durability;
 	}
+
 	public void setDurability(int durability) {
 		this.durability = durability;
 	}
+
 	public int getEnergy() {
 		return energy;
 	}
+
 	public void setEnergy(int energy) {
 		this.energy = energy;
 	}
+
 	public int getFightingSkills() {
 		return fightingSkills;
 	}
+
 	public void setFightingSkills(int fightingSkills) {
 		this.fightingSkills = fightingSkills;
+	}
+
+	public int getSpeedy() {
+		return speedy;
+	}
+
+	public void setSpeedy(int speedy) {
+		this.speedy = speedy;
 	}
 }
--- src/br/com/herochoice/Chooser.java
+++ src/br/com/herochoice/Chooser.java
@@ -3,6 +3,8 @@ package br.com.herochoice;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Chooser {
 
@@ -10,21 +12,44 @@ public class Chooser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			readCsv();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	@SuppressWarnings("unused")
-	public static Object readCsv() throws IOException {
-		br = new BufferedReader(new FileReader("test.csv"));
+	public static List<Character> readCsv() throws IOException {
+		br = new BufferedReader(new FileReader("character.csv"));
 
 		String line;
-		String splitBy = ",";
+		String splitBy = ";";
+		List<Character> characteres = new ArrayList<Character>();
+		boolean firstLine = true;
 		while ((line = br.readLine()) != null) {
+			if(firstLine){
+				firstLine = false;
+				continue;
+			}
+			
 			String[] b = line.split(splitBy);
-			// TODO Percorrer a matriz setando os dados para cada heroi
-
+			
+			Character character = new Character();
+			character.setId(Integer.parseInt(b[0]));
+			character.setName(b[1]);
+			character.setHero(b[2].equals("hero") ? true : false);
+			character.setIntelligence(Integer.parseInt(b[3]));
+			character.setStrength(Integer.parseInt(b[4]));
+			character.setSpeedy(Integer.parseInt(b[5]));
+			character.setDurability(Integer.parseInt(b[6]));
+			character.setEnergy(Integer.parseInt(b[7]));
+			character.setFightingSkills(Integer.parseInt(b[8]));
+			character.setNumberComics(Integer.parseInt(b[9]));
+			
+			characteres.add(character);
 		}
-		return line;
+			
+		return characteres;
 	}
-
 }
