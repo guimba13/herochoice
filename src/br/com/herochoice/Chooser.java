71169cb1847a0b6d99dd46f6116af759b1cff106
Guilherme Zanelato <guilherme.c.zanelato@gmail.com>
2014-12-05 22:11:08 -0200

    adicionando leitura de arquivo csv

--- src/br/com/herochoice/Chooser.java
+++ src/br/com/herochoice/Chooser.java
@@ -1,10 +1,30 @@
 package br.com.herochoice;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Chooser {
+
+	private static BufferedReader br;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@SuppressWarnings("unused")
+	public static Object readCsv() throws IOException {
+		br = new BufferedReader(new FileReader("test.csv"));
+
+		String line;
+		String splitBy = ",";
+		while ((line = br.readLine()) != null) {
+			String[] b = line.split(splitBy);
+			// TODO Percorrer a matriz setando os dados para cada heroi
+
+		}
+		return line;
 	}
 
 }
