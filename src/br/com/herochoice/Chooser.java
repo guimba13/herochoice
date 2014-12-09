f48205352764ff69406d390851f4feeb9f3fa2ff
Guilherme Zanelato <guilherme.c.zanelato@gmail.com>
2014-12-08 22:25:05 -0200

    adicionando leitura de vilões

--- src/br/com/herochoice/Chooser.java
+++ src/br/com/herochoice/Chooser.java
@@ -1,8 +1,10 @@
 package br.com.herochoice;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
-
 
 public class Chooser {
 
@@ -10,10 +12,17 @@ public class Chooser {
 	private static Relation relations[][];
 
 	public static void main(String[] args) {
-		
+
 		try {
 			characters = Helper.readCharacter();
 			relations = Helper.readRelations();
+
+			List<String> villains = Files.readAllLines(Paths.get(args[0]),
+					StandardCharsets.UTF_8);
+
+			String villainsIds[] = villains.get(0).split(" ");
+
+			return;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -26,13 +35,12 @@ public class Chooser {
 	
 	
 
-	private static Character getCharacterById(int id){
-		return characters.get(id-1);
-	}
-	
-	private static Relation getRelationsById(int id1, int id2){
-		return relations[id1-1][id2-1];
+	private static Character getCharacterById(int id) {
+		return characters.get(id - 1);
 	}
 
-	
+	private static Relation getRelationsById(int id1, int id2) {
+		return relations[id1 - 1][id2 - 1];
+	}
+
 }
