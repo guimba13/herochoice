1c63dd5a89fcaf134246faaf917f260d998d489f
Guilherme Zanelato <guilherme.c.zanelato@gmail.com>
2014-12-09 00:04:59 -0200

    tirando geracoes aleatorias repetidas

--- src/br/com/herochoice/Team.java
+++ src/br/com/herochoice/Team.java
@@ -162,14 +162,56 @@ public class Team {
 		team1.setTeam(team2.getTeam());
 	}
 	
+	private Boolean IsInTeam(List<Character> team, Character c){
+		for(Character cs : team){
+			if(c.getId()==cs.getId()){
+				return Boolean.TRUE;
+			}
+		}
+		return Boolean.FALSE;
+	}
+	
 	public Team addCharacter(){
 		Random rand = new Random();
-		int newHero = rand.nextInt(381) + 1; //382 a 777
+		int newHero = rand.nextInt(381) + 1;
+		Character newChar;
+		do{
+			newChar = Helper.characters.get(newHero-1);
+		}while(!IsInTeam(team, newChar));
 		Team newTeam = new Team();
 		copyTeam(newTeam, this);
 		
-		newTeam.getTeam().add(Helper.characters.get(newHero));
+		newTeam.getTeam().add(newChar);
 		newTeam.setSize(newTeam.getSize()+1);
+		newTeam.calculateAvgs();
+		return newTeam;
+	}
+	
+	public Team removeCharacter(){
+		Random rand = new Random();
+		int newHero = rand.nextInt(this.size) + 1; 
+		Team newTeam = new Team();
+		copyTeam(newTeam, this);
+		
+		newTeam.getTeam().remove(newHero-1);
+		newTeam.setSize(newTeam.getSize()-1);
+		newTeam.calculateAvgs();
+		return newTeam;
+	}
+	
+	public Team changeCharacter(){
+		Random rand = new Random();
+		int heroOut = rand.nextInt(this.size) + 1; 
+		int heroIn = rand.nextInt(381) + 1; 
+		Character newChar;
+		do{
+			newChar = Helper.characters.get(heroIn-1);
+		}while(!IsInTeam(team, newChar));
+		Team newTeam = new Team();
+		copyTeam(newTeam, this);
+		
+		newTeam.getTeam().remove(heroOut-1);
+		newTeam.getTeam().add(newChar);
 		newTeam.calculateAvgs();
 		return newTeam;
 	}
